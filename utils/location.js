451a6f9ea94e9780e23481159fce47a7fab56dbd
rajkumar kushwah <rajkumarkushwah3262@gmail.com>
2026-02-04 14:52:04 +0530

    location again update

--- utils/location.js
+++ utils/location.js
@@ -1,14 +1,29 @@
 import fetch from "node-fetch";
 
+// export async function reverseGeocode(lat, lng) {
+//   const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`;
+
+//   const res = await fetch(url, {
+//     headers: {
+//       "User-Agent": "Attendance-System"
+//     }
+//   });
+
+//   const data = await res.json();
+//   return data.display_name || "Unknown location";
+// }
+
+
 export async function reverseGeocode(lat, lng) {
-  const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`;
+  const apiKey = process.env.GOOGLE_MAPS_API_KEY; // must be set
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${apiKey}`;
 
-  const res = await fetch(url, {
-    headers: {
-      "User-Agent": "Attendance-System"
-    }
-  });
-
+  const res = await fetch(url);
   const data = await res.json();
-  return data.display_name || "Unknown location";
+
+  if (data.status === "OK") {
+    return data.results[0].formatted_address; // gives building, street, landmarks
+  } else {
+    return "Unknown location";
+  }
 }
