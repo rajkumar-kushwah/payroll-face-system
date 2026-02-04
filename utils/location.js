d4391792460e9b6cc5e120887c59f139ca09f9e1
rajkumar kushwah <rajkumarkushwah3262@gmail.com>
2026-02-04 12:32:22 +0530

    location

--- utils/location.js
+++ utils/location.js
@@ -1,32 +1,14 @@
 import fetch from "node-fetch";
 
-// export async function reverseGeocode(lat, lng) {
-//   const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`;
-
-//   const res = await fetch(url, {
-//     headers: {
-//       "User-Agent": "Attendance-System"
-//     }
-//   });
-
-//   const data = await res.json();
-//   return data.display_name || "Unknown location";
-// }
-
-
 export async function reverseGeocode(lat, lng) {
-  const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}&zoom=18&addressdetails=1`;
+  const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`;
 
   const res = await fetch(url, {
-    headers: { "User-Agent": "Attendance-System" }
+    headers: {
+      "User-Agent": "Attendance-System"
+    }
   });
 
   const data = await res.json();
-
-  return (
-    data.address?.building ||
-    data.address?.road ||
-    data.display_name ||
-    "Unknown location"
-  );
+  return data.display_name || "Unknown location";
 }
